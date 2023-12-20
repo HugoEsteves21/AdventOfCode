d6bc6f91572acab7d7ffc644e0990bd9413811fa
Hugo <hugo.veigaa@gmail.com>
2023-12-20 18:14:49 +0000

    work on day 2

--- day2/index.js
+++ day2/index.js
@@ -13,23 +13,45 @@ const spelledNumbers = {
   nine: '9',
 };
 
+const input = [
+  'two1nine',
+  'eightwothree',
+  'abcone2threexyz',
+  'xtwone3four',
+  '4nineeightseven2',
+  'zoneight234',
+  '7pqrstsixteen',
+];
+
 const transformNumbers = (string) => {
   Object.entries(spelledNumbers).forEach((obj) => {
+    const regex = new RegExp(obj[0], 'g');
     if (string.includes(obj[0])) {
-      let newString = string.replace(obj[0], obj[1]);
-      console.log(newString);
-      return newString;
+      string = string.replace(regex, obj[1]);
     }
   });
+  return string;
 };
 
-const numberSum = INPUT.map((line) => {
-  line = transformNumbers(line);
-  let fullNum = line.match(/\d/g);
+const mapNumbers = (string) => {
+  Object.keys(spelledNumbers).forEach((key) => {
+    const regex = new RegExp(`\\b${key}\\b`, 'g');
+    string = string.replace(regex, spelledNumbers[key]);
+  });
+  return string;
+};
+
+const numberSum = input.map((line) => {
+  console.log(`inside numberSum: ${line}`);
+  let newLine = transformNumbers(line);
+  console.log(`this is the new line: ${newLine}`);
+  let fullNum = newLine.match(/\d/g);
+  console.log(fullNum);
   fullNum = fullNum.join('');
   let num = fullNum.slice(0, 1) + fullNum.slice(-1);
+  console.log(`NUMBER TO SUM: ${num}`);
   finalSum += parseInt(num);
   return finalSum;
 });
 
-console.log(finalSum);
+console.log(`This is the final sum: ${finalSum}`);
