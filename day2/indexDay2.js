1b3ab195d7f7b297c535ef868ebc1a13b9a553f6
Hugo Esteves <hugo.veigaa@gmail.com>
2023-12-28 12:25:56 +0000

    finish Day2

--- day2/indexDay2.js
+++ day2/indexDay2.js
@@ -1,1 +1,50 @@
-import { INPUT } from "./inputDay3.js";
+import { INPUT } from "./inputDay2.js";
+
+const createInput = () => {
+  const NEWINPUT = new Object();
+
+  Object.keys(INPUT).map((game) => {
+    const firstArray = INPUT[game].split("; ");
+    NEWINPUT[game] = [];
+    const arrayWithSets = new Array();
+
+    firstArray.map((sets) => {
+      arrayWithSets.push(sets);
+    });
+
+    arrayWithSets.map((set) => {
+      const setObj = new Object();
+      const setArr = set.split(", ");
+
+      setArr.map((cube) => {
+        setObj[cube.split(" ")[1]] = cube.split(" ")[0];
+      });
+      NEWINPUT[game].push(setObj);
+    });
+  });
+  return NEWINPUT;
+};
+
+const countIds = () => {
+  const data = createInput();
+  let gameCounter = 0;
+
+  Object.keys(data).map((game) => {
+    let passCondition = 0;
+    data[game].map((set) => {
+      if (
+        (set.blue <= 14 || !set.hasOwnProperty("blue")) &&
+        (set.red <= 12 || !set.hasOwnProperty("red")) &&
+        (set.green <= 13 || !set.hasOwnProperty("green"))
+      ) {
+        passCondition++;
+      }
+    });
+    if (passCondition === data[game].length) {
+      gameCounter += Number(game);
+    }
+  });
+  console.log(gameCounter);
+};
+
+countIds();
